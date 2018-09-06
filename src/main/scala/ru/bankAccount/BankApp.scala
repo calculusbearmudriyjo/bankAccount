5b4453d50a2c55f9bf02e51cc1b3263c31323dd5
Alexander Babin <mudriyjo@gmail.com>
2018-09-06 17:47:15 +0300

    impliment base api. add account/fill account/transfer money/show account list

--- src/main/scala/ru/bankAccount/BankApp.scala
+++ src/main/scala/ru/bankAccount/BankApp.scala
@@ -3,25 +3,67 @@ package ru.bankAccount
 import com.twitter.finagle.Http
 import com.twitter.logging.Logging
 import com.twitter.server.TwitterServer
-import com.twitter.util.{Await, Future}
+import com.twitter.util.Await
 import com.typesafe.config.ConfigFactory
 import io.finch._
 import io.finch.circe._
 import io.finch.syntax._
 import io.circe.generic.auto._
 import ru.bankAccount.config.Config.ServerConfig
-import ru.bankAccount.service.AccountService
+import ru.bankAccount.service.{Account, AccountService}
 
 
 object BankApp extends TwitterServer with Logging {
   private lazy val config = ConfigFactory.load()
   private lazy val serverConf = config.getConfig("ru.bankAccount.server")
   private lazy val accountService = AccountService()
-  case class Response(text: String)
 
-  val accountEndpoint: Endpoint[Response] =
+  case class addAccountRequest(name: String) extends AnyVal
+  case class fillAccountRequest(amount: Long)
+  case class TransferRequest(to: String, amount: Long)
+
+  val listAccountEndpoint: Endpoint[List[Account]] =
     get("account") {
-     Ok(Response(accountService.test()))
+      for {
+        r <- accountService.getAccount()
+      } yield Ok(r)
+    }
+
+  val getAccountEndpoint: Endpoint[Option[Account]] =
+    get("account" :: path[String]) { req: String =>
+      for {
+        r <- accountService.getAccount(req)
+      } yield Ok(r)
+    }
+
+  val addAccountEndpoint: Endpoint[Account] =
+    post("account" :: jsonBody[addAccountRequest]) { req: addAccountRequest =>
+      for {
+        r <- accountService.createAccount(req.name)
+      } yield r match {
+        case Right(acc) => Ok(acc)
+        case Left(msg) => BadRequest(msg)
+      }
+    }
+
+  val fillAccountEndpoint: Endpoint[Account] =
+    post("account" :: path[String] :: "fill" :: jsonBody[fillAccountRequest]) { (name: String, req: fillAccountRequest) =>
+      for {
+        r <- accountService.fillAccount(name, req.amount)
+      } yield r match {
+        case Right(acc) => Ok(acc)
+        case Left(msg) => BadRequest(msg)
+      }
+    }
+
+  val transferAccountEndpoint: Endpoint[Account] =
+    post("account" ::path[String] :: "transfer" :: jsonBody[TransferRequest]) { (from:String, req: TransferRequest) =>
+      for {
+        r <- accountService.transferAmount(from, req.to, req.amount)
+      } yield r match {
+        case Right(acc) => Ok(acc)
+        case Left(msg) => BadRequest(msg)
+      }
     }
 
   def main() {
@@ -31,7 +73,12 @@ object BankApp extends TwitterServer with Logging {
       .withAdmissionControl.concurrencyLimit(
       maxConcurrentRequests = 400,
       maxWaiters = 100
-    ).serve(s"${server.host}:${server.port}", accountEndpoint.toService)
+    ).serve(s"${server.host}:${server.port}",
+      (listAccountEndpoint :+:
+      getAccountEndpoint :+:
+      addAccountEndpoint :+:
+      fillAccountEndpoint :+:
+      transferAccountEndpoint).toService)
 
     onExit{
       app.close()
--- src/main/scala/ru/bankAccount/service/AccountService.scala
+++ src/main/scala/ru/bankAccount/service/AccountService.scala
@@ -1,23 +1,63 @@
 package ru.bankAccount.service
 
 import com.twitter.util.Future
-
 import scala.concurrent.stm._
 
 case class Account(name: String, amount: Long)
 
+final case class AccountException(message: String) extends Exception
+final case class TransferException(message: String) extends Exception
+final case class FillAccountException(message: String) extends Exception
+
 class AccountService {
-  val accounts: Map[String, Ref[Account]] = Map()
+  var accounts: Map[String, Ref[Account]] = Map()
 
-  def createAccount(name: String): Future[Account] = Future {
-    val newAccount = Ref(Account(name, 0L))
-    accounts + (name -> newAccount)
-    newAccount.single()
+  def getAccount(): Future[List[Account]] = Future{accounts.values.map(_.single.get).toList}
+  def getAccount(name: String): Future[Option[Account]] = Future{accounts.get(name).map(_.single.get)}
+
+  def createAccount(name: String): Future[Either[Exception,Account]] = Future {
+    if(accounts.get(name).isEmpty) {
+      val newAccount = Ref(Account(name, 0L))
+      accounts = accounts + (name -> newAccount)
+      Right(newAccount.single())
+    } else {
+      Left(AccountException("account exist"))
+    }
   }
-  
-//  def
 
-  def test(): String = "Hello, world"
+  def transferAmount(from: String, to: String, amount: Long): Future[Either[Exception,Account]] = Future {
+    accounts.get(from) match {
+        case None => Left(TransferException(s"account from not found"))
+        case Some(f) => accounts.get(to) match {
+          case None => Left(TransferException(s"account to not found"))
+          case Some(t) => {
+            var res: Either[Exception,Account] = Left(TransferException("cannot transfer money from :" + f.single().name + ", to :" + t.single().name))
+            atomic { implicit trx =>
+              if(f().amount - amount > 0) {
+                f() = Account(f().name, f().amount - amount)
+                t() = Account(t().name, t().amount + amount)
+                res = Right(f())
+              }
+            }
+            res
+          }
+      }
+    }
+  }
+
+  def fillAccount(name: String, amount: Long): Future[Either[Exception,Account]] = Future {
+    accounts.get(name) match {
+      case None => Left(FillAccountException("fill account not found"))
+      case Some(a) => {
+        var res: Either[Exception,Account] = Left(TransferException("cannot fill account, trx rollback"))
+        atomic {implicit trx =>
+          a() = Account(a().name, a().amount + amount)
+          res = Right(a())
+        }
+        res
+      }
+    }
+  }
 }
 
 object AccountService {
